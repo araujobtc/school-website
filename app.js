2628ab64337d6787b2c57029d785e4be1379610e
Isabelle Araújo <isabelletecn@gmail.com>
2022-01-17 04:10:40 -0300

    index 1

--- app.js
+++ app.js
@@ -13,10 +13,6 @@ app.get('/', function(req, res){
     res.render('index');
 });
 
-app.get('/about', function(req, res){
-    res.render('about');
-});
-
 app.get('/login', function(req, res){
     res.render('login');
 });
